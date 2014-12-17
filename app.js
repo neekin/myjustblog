df11a0b432fe44f54ef032972abc7c0d2d8eae0a
NeeKin <njgzs360@gmail.com>
2014-12-17 22:16:59 +0800

    include baidu-ueditor

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ var express = require("express");
 var bodyParser = require("body-parser");
 //引入path  因为我们引入样式后，框架并不知道我们的静态资源文件在哪
 var path = require("path");
+//引入百度富文本
+var ueditor = require("ueditor");
 //1引入mongoose ODM来操作数据
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 //2mongoose链接服务器
-mongoose.connect('mongodb://localhost/local');
+//mongoose.connect('mongodb://localhost/local');
 //3引入model模型
-var Article = require("./model/article");
+//var Article = require("./model/article");
 //4再来初始化对象
-var article = new Article({
+/*var article = new Article({
 	title:"这仅仅是个测试",
 	subtitle:"没错 是个测试",
 	articlebody:"一下写到这 我好紧张呀"
-});
+});*/
 
 //5最后保存即可
 /*   article.save(function(err){
@@ -33,6 +35,25 @@ app.set("view engine","jade");
 app.use(express.static(path.join(__dirname,"")));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
+app.use("/public/ueditor/ue", ueditor(path.join(__dirname, "images"), function(req, res, next) {
+  // ueditor 客户发起上传图片请求
+  if(req.query.action === 'uploadimage'){
+    var foo = req.ueditor;
+    var date = new Date();
+    var imgname = req.ueditor.filename;
+
+    var img_url = date.getTime()+imgname;
+    
+    res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+  }//  客户端发起图片列表请求
+  else if (req.query.action === 'listimage'){
+    var dir_url = '/';
+    res.ue_list(dir_url);  // 客户端会列出 dir_url 目录下的所有图片
+  }else {
+
+    res.setHeader('Content-Type', 'application/json');
+    res.redirect('/public/ueditor/nodejs/config.json')
+}}));
 //启动应用监听
 app.listen(port);
 console.log("Website is on prot "+ port);
@@ -56,3 +77,4 @@ app.post("/add",function(req,res){
 app.get("/test",function  (req,res) {
 	res.render("test",{});
 });
+
---
+++ js/baidu-ueditor-init.js
@@ -0,0 +1,3 @@
+   (function(){
+ var ue = UE.getEditor('editor');
+})()
